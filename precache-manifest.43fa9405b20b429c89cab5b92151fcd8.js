e0349a675f7998357a7d3f5bd64c48291ab68567
mingkang <49049296+kangming1412@users.noreply.github.com>
2020-05-06 17:24:14 -0700

    Add files via upload

---
+++ precache-manifest.43fa9405b20b429c89cab5b92151fcd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3a68a8072dfe578f35e60c34a1d750",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bcc8dc74c8825c812b0e",
+    "url": "./static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "./static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "bcc8dc74c8825c812b0e",
+    "url": "./static/js/main.01a9c41d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
